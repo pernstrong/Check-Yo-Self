2278cf158202e567dca1ee484f76729ed31ccd0b
Dave Pernitz <pernitz@uwalumni.com>
2020-02-29 18:12:58 -0700

    Add tasks to populate on cards list

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ var aside = document.querySelector('.aside')
 var numberOfLists = -1;
 var numberOfTasks = -1;
 var currentTasks = [];
-var allToDoLists = []
+var allToDoLists = [];
+// var totalNumOfLists;
+var numOfListsSaved;
 
 aside.addEventListener('click', routeAsideFunctions)
 
@@ -47,15 +49,17 @@ function routeAsideFunctions(event) {
   }
 
 function createToDoList() {
-  numberOfLists++
+  // numberOfLists++
   currentTasksString = JSON.stringify(currentTasks)
-  var toDoList = new ToDoList(`${numberOfLists}`, `${toDoTitle.value}`, `${currentTasksString}`);
+  var toDoList = new ToDoList(Date.now(), `${toDoTitle.value}`, `${currentTasksString}`);
   // console.log(toDoList.id)
   // console.log(toDoList.title)
   // console.log(toDoTitle.value)
   allToDoLists.push(toDoList)
-  displayLists()
+  // displayLists()
+  // var title = toDoTitle.value
   toDoList.saveToStorage();
+  displayList(toDoList)
   toDoTitle.value = '';
   currentTasks = []
 }
@@ -83,12 +87,12 @@ function displayTaskOnAside() {
 
 
 function deleteTaskOnAside(event) {
-  console.log(currentTasks)
-  console.log(event.target)
+  // console.log(currentTasks)
+  // console.log(event.target)
   buttonSelectedIndex = event.target.dataset.tasknum
-  console.log(buttonSelectedIndex)
+  // console.log(buttonSelectedIndex)
   currentTasks.splice(buttonSelectedIndex, 1)
-  console.log(currentTasks)
+  // console.log(currentTasks)
   displayTaskOnAside()
 }
 
@@ -102,62 +106,143 @@ function clearTasksOnAside(event) {
   // console.log(currentTasks)
 }
 
-function displayLists() {
-  // message = document.querySelector('.no-task-message')
-  // message.classList.add('hide')
-  columnOne = document.querySelector('.column-one');
-  columnTwo = document.querySelector('.column-two');
-  for (var i = 0; i < allToDoLists.length; i++) {
-    currentList  = allToDoLists[i];
-    // debugger
-    console.log(currentList)
-    // currentListFormatted = JSON.parse(currentList)
-    // console.log(currentListFormatted)
-    console.log(currentList.tasks[0])
-    var targetColumn;
-    if (i % 2 === 0) {
-      targetColumn = columnOne;
-    } else {
-      targetColumn = columnTwo;
-    }
-    // debugger
-    // columnOne.insertAdjacentHTML('beforeend',
-    console.log(currentList.tasks[i])
-    targetColumn.innerHTML =
-    `<div class="task-card">
-      <h4 class="task-card-title">${currentList.title}</h4>
-      <ul class="task-card-list">
-        <!-- <li><input type="image" src="assets/images/checkbox.svg">${currentList.tasks[i].objective}</li>
-        <li><input type="image" src="assets/images/checkbox.svg">Every chance I get, I water the plants</li>
-        <li><input type="image" src="assets/images/checkbox.svg">Lion! Cloth talk</li> -->
-      </ul>
-      <section class="urgent-button-section">
-        <input type="image" src="assets/images/urgent.svg" class="task-card-urgent-button">
-        <p>URGENT</p>
-      </section>
-      <section class="delete-button-section">
-      <input type="image" src="assets/images/delete.svg" class="task-card-delete-button">
-      <p>DELETE</p>
-      </section>
-    </div>`
-  // )
+function displayList(toDoList) {
+    columnOne = document.querySelector('.column-one');
+    columnTwo = document.querySelector('.column-two');
+      // currentList  = allToDoLists[i];
+      var targetColumn;
+      // for (var i = 0; i < 1; i++) {
+      if (allToDoLists.length === 1) {
+        columnOne.innerHTML = ''
+      }
+      if (allToDoLists.length % 2 === 0) {
+        targetColumn = columnTwo;
+      } else {
+        targetColumn = columnOne;
+      }
+      targetColumn.insertAdjacentHTML('afterbegin',
+      `<div class="task-card">
+        <h4 class="task-card-title">${toDoList.title}</h4>
+        <ul class="task-card-list${toDoList.id}">
+
+        </ul>
+        <section class="urgent-button-section">
+          <input type="image" src="assets/images/urgent.svg" class="task-card-urgent-button">
+          <p>URGENT</p>
+        </section>
+        <section class="delete-button-section">
+        <input type="image" src="assets/images/delete.svg" class="task-card-delete-button">
+        <p>DELETE</p>
+        </section>
+      </div>`
+    )
+    // var taskCardList = document.querySelector(`.task-card-list${i}`)
+    displayTasksInCards(toDoList);
   }
-  asideTaskListArea = document.querySelector('.aside-task-list-area')
-  asideTaskListArea.innerHTML = ''
-  // displayTasksOnList()
+// }
+  function displayTasksInCards(toDoList){
+    taskCardList = document.querySelector(`.task-card-list${toDoList.id}`)
+    taskCardList.innerHTML = ''
+    for (var i = 0; i < currentTasks.length; i++) {
+      taskCardList.insertAdjacentHTML('beforeend',
+      `<li><input type="image" src="assets/images/checkbox.svg">${currentTasks[i].objective}</li>
+      `
+    )
+
+    asideTaskListArea = document.querySelector('.aside-task-list-area')
+    asideTaskListArea.innerHTML = ''
+    }
 }
 
-// function displayTasksOnList() {
-//   // console.log(currentList.tasks.objective)
-//   var taskCardList = document.querySelector('.task-card-list')
-//   for (var i = 0; i < currentTasks.length; i++) {
-//     // var currentTask = JSON.parse('currentList.tasks[i].objective')
-//     taskCardList.innerHTML =
-//     `<li>${currentTask}</li>
-//     `
+
+
+
+
+
+// function displayLists() {
+//   // message = document.querySelector('.no-task-message')
+//   // message.classList.add('hide')
+//   columnOne = document.querySelector('.column-one');
+//   columnTwo = document.querySelector('.column-two');
+//   columnOne.innerHTML = ''
+//   columnTwo.innerHTML = ''
+//   for (var i = 0; i < allToDoLists.length; i++) {
+//     currentList  = allToDoLists[i];
+//     // debugger
+//     // console.log(currentList)
+//     // currentListFormatted = JSON.parse(currentList)
+//     // console.log(currentListFormatted)
+//     // console.log(currentList.tasks[0])
+//     var targetColumn;
+//     if (i % 2 === 0) {
+//       targetColumn = columnOne;
+//     } else {
+//       targetColumn = columnTwo;
+//     }
+//     targetColumn.insertAdjacentHTML('afterbegin',
+//     `<div class="task-card">
+//       <h4 class="task-card-title">${currentList.title}</h4>
+//       <ul class="task-card-list${i}" data-cardnum="${i}">
 //
+//       </ul>
+//       <section class="urgent-button-section">
+//         <input type="image" src="assets/images/urgent.svg" class="task-card-urgent-button">
+//         <p>URGENT</p>
+//       </section>
+//       <section class="delete-button-section">
+//       <input type="image" src="assets/images/delete.svg" class="task-card-delete-button">
+//       <p>DELETE</p>
+//       </section>
+//     </div>`
+//   )
+//   currentUL = document.querySelector(`.task-card-list${i}`)
+//   console.log(currentUL)
+//   console.log(currentList)
+//   displayTasksOnList(currentUL, currentList)
 //   }
+//   asideTaskListArea = document.querySelector('.aside-task-list-area')
+//   asideTaskListArea.innerHTML = ''
 // }
+
+// function displayTasksOnList(currentUL, currentList) {
+  // console.log(currentList.tasks.objective)
+  // console.log(currentTasks.length)
+  // // var taskCardList = document.querySelector('.task-card-list')
+  // // taskCardList.innerHTML = ``
+  // for (var i = 0; i < currentTasks.length; i++) {
+  //   var currentTask = currentTasks[i]
+  //   console.log(currentTask)
+  //   currentUL.insertAdjacentHTML('beforeend',
+  //   `<li><input type="image" src="assets/images/checkbox.svg">${currentTask.objective}</li>
+  //   `
+  // )}
+// }
+
+// well, i'm thinking we need to know how many lists have been saved in order to load the correct number?
+// function storeNumOfLists(numOfListsSaved) {
+//   totalNumOfLists = numOfListsSaved
+//   loadTasksFromStorage()
+// }
+
+// get allToDoLists out of local storage and display on page load
+
+window.addEventListener('load', loadTasksFromStorage)
+
+function loadTasksFromStorage() {
+  // debugger
+  allToDoLists = [];
+  numOfListsSaved = JSON.parse(localStorage.getItem('numOfListsSaved'));
+  // console.log(numOfListsSaved)
+  for (var i = 0; i < numOfListsSaved; i++) {
+    var list = JSON.parse(localStorage.getItem(`'currentList${i}'`))
+    // console.log(list)
+    allToDoLists.push(list)
+  }
+  // console.log(numOfListsSaved)
+  // console.log(allToDoLists)
+  // displayLists()
+  // displayList()
+}
 
 
 
--- todo-list.js
+++ todo-list.js
@@ -10,18 +10,27 @@ class ToDoList {
   }
   saveToStorage() {
     // save to local storage
+    var numOfListsSaved = 0
     for (var i = 0; i < allToDoLists.length; i++) {
+      numOfListsSaved++
       var currentList = allToDoLists[i];
       localStorage.setItem(`'currentList${i}'`, JSON.stringify(currentList))
     }
+    numOfListsSaved = allToDoLists.length
+    // console.log(numOfListsSaved)
+    localStorage.setItem('numOfListsSaved', JSON.stringify(numOfListsSaved))
+    // storeNumOfLists(numOfListsSaved)
+    loadTasksFromStorage();
   }
+
+
   deleteFromStorage() {
 // delete from local storage
 
 
   }
   updateToDo() {
-    
+
 // should update the todo's title and urgency
   }
   updateTask() {
